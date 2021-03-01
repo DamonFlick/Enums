1e230065b23f61b2236edd982b480e3471802194
DamonFlick <75501983+DamonFlick@users.noreply.github.com>
2021-03-01 12:42:00 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeration
+{
+    class Program
+    {
+        enum daysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Sat,
+            Sunday
+        };
+
+      
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week");
+
+                Enum.TryParse(Console.ReadLine(), out daysOfTheWeek day);
+                
+                
+                Console.WriteLine(day.ToString());
+                Console.WriteLine((int)day);
+                Console.ReadLine();
+
+            }
+            catch (Exception )
+            {
+                Console.WriteLine("Please enter an actual day of the week in english.");
+                Console.ReadLine();
+            }
+
+
+        }
+    }  
+}
